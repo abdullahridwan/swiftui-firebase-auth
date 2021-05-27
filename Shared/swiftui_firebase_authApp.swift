a93eb7ddc4dbd539087b6a1f66c6c80c7ef32586
Abdullah Ridwan <abdullahridwan73@gmail.com>
2021-05-27 17:42:38 -0400

    Initial Commit

---
+++ Shared/swiftui_firebase_authApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_firebase_authApp.swift
+//  Shared
+//
+//  Created by Abdullah Ridwan on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_firebase_authApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
